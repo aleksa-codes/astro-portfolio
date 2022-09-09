21640fe82f76a93ed31d3c8162bae8466728e4d2
aleksastojsic <stojsic.aleksa@gmail.com>
2022-09-09 19:17:21 +0200

    Feat: typewriter effect and wave hand

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,10 +8,36 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['"Poppins"', 'sans-serif']
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' }
+        },
+        fadein: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' }
+        }
+      },
+      animation: {
+        'waving-hand': 'wave 2s linear infinite 2s',
+        'content-fadein': 'fadein 2s linear infinite'
       }
     }
   },
   daisyui: {
-    themes: ['winter', 'night']
+    themes: ['night', 'winter']
   }
 };
