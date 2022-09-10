f82ae04e7fcc5a1d5dbc0e0e8fdea14b96464ff6
aleksastojsic <stojsic.aleksa@gmail.com>
2022-09-10 22:37:33 +0200

    Fix: astro config imports reorder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 
 // https://astro.build/config
