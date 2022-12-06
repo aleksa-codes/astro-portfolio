3e5f9fca320bab7c5cd55dba45a5e388416eb2ad
aleksa-codes <stojsic.aleksa@gmail.com>
2022-12-06 20:13:30 +0100

    Fix: og image for posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
-import astroOGImage from 'astro-og-image';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,17 +13,5 @@ export default defineConfig({
       theme: 'one-dark-pro'
     }
   },
-  integrations: [
-    sitemap(),
-    mdx(),
-    tailwind(),
-    image(),
-    astroOGImage({
-      config: {
-        path: './src/posts'
-        // change this value to the folder where your posts are
-        // NOTE: index.md file will not get proccesed, so please avoid it
-      }
-    })
-  ]
+  integrations: [sitemap(), mdx(), tailwind(), image()]
 });
