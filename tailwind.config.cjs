b6dd85cae14a102ad6312f5ad080f09f219633d1
aleksa-codes <stojsic.aleksa@gmail.com>
2022-12-07 13:15:37 +0100

    Fix: trying something

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
   plugins: [
     require('tailwind-scrollbar')({ nocompatible: true }),
     require('@tailwindcss/typography'),
