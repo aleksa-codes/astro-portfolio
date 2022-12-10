7280ded3afd75f0f9c60acb99c3451d366b33557
aleksa-codes <stojsic.aleksa@gmail.com>
2022-12-10 23:25:06 +0100

    Fix: projects and contat page text

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     name: 'Next Day',
-    desc: "To-Do web application to manage task lists and tasks with the 'Pomodoro' focus timer for each of them. Users can create an account or log in with Google. CRUD operations are available for tasks and task lists. Deployed on Vercel.",
+    desc: 'A to-do web app that makes managing tasks and task lists a breeze. With the "Pomodoro" focus timer, you can stay on track and get things done. Sign up or log in with Google to start organizing your tasks. Next Day is deployed on Vercel and ready for you to use.',
     tech: ['React', 'Next.js', 'Thin.dev', 'Tailwind CSS'],
     github: 'https://github.com/aleksa-codes/next-day-todo-app',
     demo: 'https://nextday.aleksa.codes',
@@ -9,7 +9,7 @@ export const projects = [
   },
   {
     name: 'Next Meal',
-    desc: "Progressive Web App that generates a random meal with a recipe, giving you ideas when you can't decide what to eat. Application is fetching data from the 'TheMealDB' API. Deployed on Vercel and installable on any device.",
+    desc: "Feeling stuck and can't decide what to eat? Next Meal has you covered. This progressive web app generates a random meal with a recipe, giving you endless inspiration. It's built with modern technologies, deployed on Vercel, and can be installed on any device.",
     tech: ['React', 'Next.js', 'Tailwind CSS', 'daisyUI'],
     github: 'https://github.com/aleksa-codes/my-next-meal-pwa',
     demo: 'https://nextmeal.aleksa.codes',
@@ -17,7 +17,7 @@ export const projects = [
   },
   {
     name: 'Hello Fresh Clone',
-    desc: 'Clone of the Hello Fresh, meal service, website. Practiced using Aastro for static site generation and Tailwind CSS for styling. Deployed on GitHub Pages.',
+    desc: "A clone of the popular Hello Fresh meal service website. This project was built using Astro and Tailwind CSS, and is deployed on GitHub Pages. It's a great example of what you can create with these technologies.",
     tech: ['Astro', 'HTML', 'Tailwind CSS', 'TypeScript'],
     github: 'https://github.com/aleksa-codes/hello-fresh-clone',
     demo: 'https://aleksa-codes.github.io/hello-fresh-clone',
