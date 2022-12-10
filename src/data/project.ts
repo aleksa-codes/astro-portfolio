e1173d3d377d82d1a7dd17ab60deaa2c7420b4b1
aleksa-codes <stojsic.aleksa@gmail.com>
2022-12-10 23:30:33 +0100

    Fix: projects page text

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     name: 'Next Day',
-    desc: 'A to-do web app that makes managing tasks and task lists a breeze. With the "Pomodoro" focus timer, you can stay on track and get things done. Sign up or log in with Google to start organizing your tasks. Next Day is deployed on Vercel and ready for you to use.',
+    desc: 'To-Do web app that makes managing tasks and task lists a breeze. Sign up or log in with Google to start organizing your tasks. Next Day is deployed on Vercel and ready for you to use.',
     tech: ['React', 'Next.js', 'Thin.dev', 'Tailwind CSS'],
     github: 'https://github.com/aleksa-codes/next-day-todo-app',
     demo: 'https://nextday.aleksa.codes',
@@ -9,7 +9,7 @@ export const projects = [
   },
   {
     name: 'Next Meal',
-    desc: "Feeling stuck and can't decide what to eat? Next Meal has you covered. This progressive web app generates a random meal with a recipe, giving you endless inspiration. It's built with modern technologies, deployed on Vercel, and can be installed on any device.",
+    desc: "Feeling stuck and can't decide what to eat? Next Meal has you covered. This progressive web app generates a random meal with a recipe, giving you endless inspiration. It's deployed on Vercel and can be installed on any device.",
     tech: ['React', 'Next.js', 'Tailwind CSS', 'daisyUI'],
     github: 'https://github.com/aleksa-codes/my-next-meal-pwa',
     demo: 'https://nextmeal.aleksa.codes',
@@ -17,7 +17,7 @@ export const projects = [
   },
   {
     name: 'Hello Fresh Clone',
-    desc: "A clone of the popular Hello Fresh meal service website. This project was built using Astro and Tailwind CSS, and is deployed on GitHub Pages. It's a great example of what you can create with these technologies.",
+    desc: "Clone of the popular Hello Fresh meal service website. This project is built using Astro and Tailwind CSS, and is deployed on GitHub Pages. It's a great example of what you can create with these technologies.",
     tech: ['Astro', 'HTML', 'Tailwind CSS', 'TypeScript'],
     github: 'https://github.com/aleksa-codes/hello-fresh-clone',
     demo: 'https://aleksa-codes.github.io/hello-fresh-clone',
