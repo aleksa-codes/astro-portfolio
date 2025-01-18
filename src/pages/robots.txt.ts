14e146a94e8062a1abab69fa62f1635c9f76010f
aleksa-codes <64015178+aleksa-codes@users.noreply.github.com>
2025-01-18 15:58:58 +0100

    fix: format robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,28 +2,28 @@ import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) =>
   `User-agent: *
-   Allow: /
+Allow: /
 
-   # Explicit rules for common LLM bots that might not attribute source data.
-   User-agent: anthropic-ai
-   User-agent: Applebot-Extended
-   User-agent: Bytespider
-   User-agent: CCBot
-   User-agent: ChatGPT-User
-   User-agent: ClaudeBot
-   User-agent: cohere-ai
-   User-agent: Diffbot
-   User-agent: FacebookBot
-   User-agent: GPTBot
-   User-agent: ImagesiftBot
-   User-agent: Meta-ExternalAgent
-   User-agent: Meta-ExternalFetcher
-   User-agent: Omgilibot
-   User-agent: PerplexityBot
-   User-agent: Timpibot
-   Allow: /
+# Explicit rules for common LLM bots that might not attribute source data.
+User-agent: anthropic-ai
+User-agent: Applebot-Extended
+User-agent: Bytespider
+User-agent: CCBot
+User-agent: ChatGPT-User
+User-agent: ClaudeBot
+User-agent: cohere-ai
+User-agent: Diffbot
+User-agent: FacebookBot
+User-agent: GPTBot
+User-agent: ImagesiftBot
+User-agent: Meta-ExternalAgent
+User-agent: Meta-ExternalFetcher
+User-agent: Omgilibot
+User-agent: PerplexityBot
+User-agent: Timpibot
+Allow: /
 
-   Sitemap: ${sitemapURL.href}`;
+Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site);
