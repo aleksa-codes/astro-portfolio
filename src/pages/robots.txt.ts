dff1bdca5733746ca3e231eb6d5e8215c829daec
aleksa-codes <stojsic.aleksa@gmail.com>
2025-01-18 15:49:05 +0100

    refactor: improve head metadata and manifesest, use more icons from old project, fix robots txt and add humans txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,29 @@
 import type { APIRoute } from 'astro';
 
-const getRobotsTxt = (sitemapURL: URL) => `
-User-agent: *
-Allow: /
+const getRobotsTxt = (sitemapURL: URL) =>
+  `User-agent: *
+   Allow: /
 
-Sitemap: ${sitemapURL.href}
-`;
+   # Explicit rules for common LLM bots that might not attribute source data.
+   User-agent: anthropic-ai
+   User-agent: Applebot-Extended
+   User-agent: Bytespider
+   User-agent: CCBot
+   User-agent: ChatGPT-User
+   User-agent: ClaudeBot
+   User-agent: cohere-ai
+   User-agent: Diffbot
+   User-agent: FacebookBot
+   User-agent: GPTBot
+   User-agent: ImagesiftBot
+   User-agent: Meta-ExternalAgent
+   User-agent: Meta-ExternalFetcher
+   User-agent: Omgilibot
+   User-agent: PerplexityBot
+   User-agent: Timpibot
+   Allow: /
+
+   Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site);
