e58d3a760d314ba379eb6ec613d1fa5efb485399
aleksa-codes <hello@aleksa.codes>
2025-12-20 14:22:53 +0100

    fix: remove explicit rules for LLM bots from robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -5,25 +5,6 @@ const getRobotsTxt = (sitemapURL: URL) =>
 Allow: /
 Disallow: /admin/
 
-# Explicit rules for common LLM bots that might not attribute source data.
-User-agent: anthropic-ai
-User-agent: Applebot-Extended
-User-agent: Bytespider
-User-agent: CCBot
-User-agent: ChatGPT-User
-User-agent: ClaudeBot
-User-agent: cohere-ai
-User-agent: Diffbot
-User-agent: FacebookBot
-User-agent: GPTBot
-User-agent: ImagesiftBot
-User-agent: Meta-ExternalAgent
-User-agent: Meta-ExternalFetcher
-User-agent: Omgilibot
-User-agent: PerplexityBot
-User-agent: Timpibot
-Disallow: /
-
 Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
